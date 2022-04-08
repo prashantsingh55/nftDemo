91f7d85d9c829a31c6e28458deb98af96ecf9edc
prashantsingh55 <thakurisinghprashant@gmail.com>
2022-04-08 08:57:12 +0545

    NFTS

---
+++ tests/integration/test_advanced_collectible_integration.py
@@ -0,0 +1,24 @@
+from brownie import network, AdvancedCollectible
+import pytest
+import time
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    get_contract,
+)
+from scripts.advanced_collectible.deploy_and_create import deploy_and_create
+
+
+def test_can_create_advanced_collectible_integration():
+    # deploy the contract
+    # create the NFT
+    # get a random breed back
+    # Arrange
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for integration testing")
+    # Act
+    advanced_collectible, creation_transaction = deploy_and_create()
+    time.sleep(60)
+
+    # Assert
+    assert advanced_collectible.tokenCounter() == 1
